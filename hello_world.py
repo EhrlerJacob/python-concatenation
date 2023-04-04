e81f0aecb6d59b5c75cd73ea896f5ddbca1396f3
MAC <mac@MacBook-Pro.lan>
2023-04-04 14:12:36 -0400

    commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+print("Hello World")
+
+name = "Noelle"
+print("Hello",name,"!")
+print("Hello "+name+"!")
+
+num = 69
+print("Hello",num,"!")
+print("Hello "+(str(num))+"!")
+
+fav_food_1 = "sushi"
+fav_food_2 = "pizza"
+print("I love to eat {} and {}".format(fav_food_1, fav_food_2))
+print(f"I love to eat {fav_food_1} and {fav_food_2}")
